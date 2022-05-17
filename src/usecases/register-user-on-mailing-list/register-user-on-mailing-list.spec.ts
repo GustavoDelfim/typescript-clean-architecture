60ad70ec346cef320a7f02f3c1c5fd62ce4a61b1
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-05-17 08:46:09 -0300

    feat: user data interface

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,15 @@
+import { UserData } from './user-data'
+
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to maling list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    // const name = 'Gustavo'
+    // const email = 'gusttavodelfim@gmail.com'
+    // const response = await usecase.resgisterUserOnMailingList({ name, email })
+    // const user = repo.findUserByEmail(email)
+    // expect((await user).name).toBe(name)
+  })
+})
