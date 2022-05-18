37602b16a768cf9a05a6b63d36cf323b5d81f516
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-05-18 08:02:46 -0300

    test: should return user if it is found in the repository

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -8,4 +8,14 @@ describe('In memory User repository', () => {
     const user = await userRepo.findUserByEmail('gusttavodelfim@gmail.com')
     expect(user).toBeNull()
   })
+
+  test('should return user if it is found in the repository', async () => {
+    const users: UserData[] = []
+    const name = 'Gustavo Delfim'
+    const email = 'gusttavodelfim@gmail.com'
+    const userRepo = new InMemoryUserRepository(users)
+    await userRepo.add({ name, email })
+    const user = await userRepo.findUserByEmail(email)
+    expect(user?.name).toBe(name)
+  })
 })
--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
@@ -8,19 +8,31 @@ export class InMemoryUserRepository implements UserRepository {
     this.repository = repository
   }
 
-  add (user: UserData): Promise<void> {
-    throw new Error('Method not implemented.')
+  async add (user: UserData): Promise<void> {
+    const exists = await this.exists(user)
+    if (!exists) {
+      this.repository.push(user)
+    }
   }
 
   async findUserByEmail (email: string): Promise<UserData | null> {
+    const user = this.repository.filter(user => user.email === email)
+    if (user.length) {
+      return user[0]
+    }
+
     return null
   }
 
-  findAllUsers (): Promise<UserData[]> {
+  async findAllUsers (): Promise<UserData[]> {
     throw new Error('Method not implemented.')
   }
 
-  exists (user: UserData): Promise<boolean> {
-    throw new Error('Method not implemented.')
+  async exists (user: UserData): Promise<boolean> {
+    const find = await this.findUserByEmail(user.email)
+    if (!find) {
+      return false
+    }
+    return true
   }
 }
