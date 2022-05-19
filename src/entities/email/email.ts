8464289ca644ef3bad39beb7da5bf9bfa811ae73
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-05-19 07:25:30 -0300

    feat: validate email with empty local part

--- src/entities/email/email.ts
+++ src/entities/email/email.ts
@@ -3,21 +3,19 @@ export class Email {
     if (!email) {
       return false
     }
-
     if (email.length > 320) {
       return false
     }
-
     const [local, domain] = email.split('@')
-
     if (local.length > 64) {
       return false
     }
-
     if (domain.length > 255) {
       return false
     }
-
+    if (!local) {
+      return false
+    }
     return true
   }
 }
