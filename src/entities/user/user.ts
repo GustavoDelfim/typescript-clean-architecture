7817badd7d19814056866e1f10c77a45a8682885
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-05-19 08:51:03 -0300

    feat: implemented invalid name in create user

--- src/entities/user/user.ts
+++ src/entities/user/user.ts
@@ -1,22 +1,28 @@
 import { Either, left, right } from '../../shared/either'
 import { Email } from '../email/email'
 import { InvalidEmailError } from '../errors/invalid-email-error'
+import { InvalidNameError } from '../errors/invalid-name-error'
+import { Name } from '../name/name'
 import { UserData } from './user-data'
 
 export class User {
-  // public name: string
-  // public email: Email
+  public readonly name: string
 
-  // constructor (userData: UserData) {
-  // }
+  private constructor (name: string) {
+    this.name = name
+  }
 
-  static create (userData: UserData): Either<InvalidEmailError, User> {
+  static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
+    const nameOrError = Name.create(userData.name)
+    if (nameOrError.isLeft()) {
+      return left(new InvalidNameError())
+    }
+
     const emailOrError = Email.create(userData.email)
-
     if (emailOrError.isLeft()) {
       return left(new InvalidEmailError())
     }
 
-    return left(new InvalidEmailError())
+    return right(new User(userData.name))
   }
 }
