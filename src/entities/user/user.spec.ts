d7a31fae951c34ee7b99b34689411b302b2d3efa
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-05-19 08:52:06 -0300

    test: validate name with many and few chars

--- src/entities/user/user.spec.ts
+++ src/entities/user/user.spec.ts
@@ -1,5 +1,6 @@
 import { left } from '../../shared/either'
 import { InvalidEmailError } from '../errors/invalid-email-error'
+import { InvalidNameError } from '../errors/invalid-name-error'
 import { User } from './user'
 
 describe('User domain entity', () => {
@@ -7,5 +8,23 @@ describe('User domain entity', () => {
     const invalidEmail = 'invalid_email'
     const error = User.create({ name: 'Gustavo Delfim', email: invalidEmail })
     expect(error).toEqual(left(new InvalidEmailError()))
+  })
+
+  test('should not create user with invalid name (too few characters)', () => {
+    const invalidName = '0      '
+    const error = User.create({
+      name: invalidName,
+      email: 'gusttavodelfim@gmail.com'
+    })
+    expect(error).toEqual(left(new InvalidNameError()))
+  })
+
+  test('should not create user with invalid name (too many characters)', () => {
+    const invalidName = '0'.repeat(257)
+    const error = User.create({
+      name: invalidName,
+      email: 'gusttavodelfim@gmail.com'
+    })
+    expect(error).toEqual(left(new InvalidNameError()))
   })
 })
