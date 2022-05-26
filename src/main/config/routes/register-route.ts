00ba0a6f16954b142c486dca406f0ce27cbdfbea
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-05-26 07:58:26 -0300

    feat: implement register router and make register route adapter

---
+++ src/main/config/routes/register-route.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+import { adaptRoute } from '@/main/config/adapters'
+
+import { makeRegisterUserController } from '@/main/factories'
+
+export default (router: Router): void => {
+  router.post('/register', adaptRoute(makeRegisterUserController()))
+}
--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,8 +1,9 @@
 import { RegisterUserController } from '@/web-controllers'
+import { WebController } from '@/web-controllers/ports'
 import { RegisterUserOnMailingList } from '@/usecases'
 import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-list/repository/in-memory-user-repository'
 
-export const makeRegisterUserController = (): RegisterUserController => {
+export const makeRegisterUserController = (): WebController => {
   const inMemoryUserRepository = new InMemoryUserRepository([])
   const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(inMemoryUserRepository)
   return new RegisterUserController(registerUserOnMailingListUseCase)
