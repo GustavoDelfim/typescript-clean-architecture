ace99dbb5812bfe24734f9298257a6ae34912900
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-05-26 08:08:55 -0300

    refactor: move folder middleware, adapter and route to main folder

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -7,7 +7,7 @@ export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
 
-  const pathRoutes = resolve(__dirname, './routes')
+  const pathRoutes = resolve(__dirname, '../routes')
   readdirSync(pathRoutes).map(async file => {
     (await import (`${pathRoutes}/${file}`)).default(router)
   })
