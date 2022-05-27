eb6351c2888ec888b064c0844f481f1aef1330fb
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-05-27 09:31:05 -0300

    chore: isntall mongodb-momery-server and dotenv

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,10 +1,12 @@
 module.exports = {
   mongodbMemoryServerOptions: {
     binary: {
-      version: '4.0.3',
+      version: '4.2.8',
       skipMD5: true
     },
     autoStart: false,
-    instance: {}
+    instance: {
+      dbName: 'jest'
+    }
   }
 }
