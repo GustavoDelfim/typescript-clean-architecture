ddd5077eea8e32268bb67ca7c8f8931af9c1e365
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-05-27 09:32:03 -0300

    feat: implement mongodb user Repository

--- src/external/repositories/mongodb/mongodb-user-repository.ts
+++ src/external/repositories/mongodb/mongodb-user-repository.ts
@@ -1,20 +1,34 @@
 import { UserData } from '@/entities/user'
 import { UserRepository } from '@/usecases/ports'
+import { MongoHelper } from './helper'
 
 export class MongodbUserRepository implements UserRepository {
-  add(user: UserData): Promise<void> {
-    throw new Error('Method not implemented.')
+  private readonly collectionName = 'users'
+
+  public async add (user: UserData): Promise<void> {
+    const userCollection = MongoHelper.getCollection(this.collectionName)
+    const exists = await this.exists(user)
+    if (!exists) {
+      await userCollection?.insertOne(user)
+    }
   }
 
-  findUserByEmail(email: string): Promise<UserData | null> {
-    throw new Error('Method not implemented.')
+  public async findUserByEmail (email: string): Promise<UserData | null> {
+    const userCollection = MongoHelper.getCollection<UserData>(this.collectionName)
+    const result = await userCollection?.findOne({ email })
+    if (!result) return null
+    return result
   }
 
-  findAllUsers(): Promise<UserData[]> {
-    throw new Error('Method not implemented.')
+  public async findAllUsers (): Promise<UserData[]> {
+    return MongoHelper.getCollection<UserData>(this.collectionName)?.find({}).toArray() || []
   }
 
-  exists(user: UserData): Promise<boolean> {
-    throw new Error('Method not implemented.')
+  public async exists (user: UserData): Promise<boolean> {
+    const found = await this.findUserByEmail(user.email)
+    if (found && found.email === user.email) {
+      return true
+    }
+    return false
   }
 }
