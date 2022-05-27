5a64d7de5c41d81a10dc5723d71489573bcf2ebc
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-05-27 09:31:39 -0300

    fix: pass type to mongodb collection

--- src/external/repositories/mongodb/helper/mongodb-helper.ts
+++ src/external/repositories/mongodb/helper/mongodb-helper.ts
@@ -4,7 +4,7 @@ interface MongoHelperInterface {
   client: null | MongoClient
   connect (uri: string): Promise<void>
   disconnect (): Promise<void>
-  getCollection (name: string): Collection | null
+  getCollection<T = any> (name: string): Collection<T> | null
   clearCollection (name: string): void
 }
 
@@ -16,7 +16,7 @@ export const MongoHelper: MongoHelperInterface = {
   async disconnect (): Promise<void> {
     await this.client?.close()
   },
-  getCollection (name: string): Collection | null {
+  getCollection<T = any> (name: string): Collection<T> | null {
     if (!this.client) return null
     return this.client?.db().collection(name)
   },
