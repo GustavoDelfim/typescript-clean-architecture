647d32a584f7a84a7a89d2715f21b78848f1b7cd
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-05-30 07:50:46 -0300

    chore: add correct port in docker mongo script

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,6 +2,8 @@ import 'dotenv/config'
 import 'module-alias/register'
 import { MongoHelper } from '@/external/repositories/mongodb/helper'
 
+console.log(process.env.MONGO_URL)
+
 MongoHelper.connect(process.env.MONGO_URL || '')
   .then(async () => {
     const app = (await import('./config/app')).default
