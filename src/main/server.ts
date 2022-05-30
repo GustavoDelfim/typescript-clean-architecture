481bd76059b69a613139221c6cd8854d1dcedc38
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-05-30 07:51:14 -0300

    refactor: remove console.log

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,8 +2,6 @@ import 'dotenv/config'
 import 'module-alias/register'
 import { MongoHelper } from '@/external/repositories/mongodb/helper'
 
-console.log(process.env.MONGO_URL)
-
 MongoHelper.connect(process.env.MONGO_URL || '')
   .then(async () => {
     const app = (await import('./config/app')).default
