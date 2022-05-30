b312073d7f5bbbaacb611e20f9487c92c7776af5
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-05-30 07:56:58 -0300

    test: coneect and disconect in mongodb

--- test/main/routes/register-route.test.ts
+++ test/main/routes/register-route.test.ts
@@ -1,7 +1,20 @@
 import request from 'supertest'
 import app from '@/main/config/app'
+import { MongoHelper } from '@/external/repositories/mongodb/helper'
 
 describe('Register Route', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL || '')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    MongoHelper.clearCollection('users')
+  })
+
   test('should return an acount on success', async () => {
     const userData = {
       name: 'Gustavo',
