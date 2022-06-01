c0ea450f7e7c2f7d81a4fa491f9f67f2bb2f7918
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-06-01 07:33:41 -0300

    feat: implement return Error

---
+++ src/external/main-services/nodemailer-email-service.ts
@@ -0,0 +1,37 @@
+import { Either, left, right } from '@/shared'
+import { MailServiceError } from '@/usecases/errors'
+import { EmailOptions, EmailService } from '@/usecases/send-email/posts/email-service'
+import * as nodemailer from 'nodemailer'
+
+export class NodemailerEmailService implements EmailService {
+  public async send (options: EmailOptions): Promise<
+    Either<
+      MailServiceError,
+      EmailOptions
+    >
+  > {
+    try {
+      const transporter = nodemailer.createTransport({
+        host: options.host,
+        port: options.port,
+        auth: {
+          user: options.username,
+          pass: options.password
+        }
+      })
+
+      await transporter.sendMail({
+        from: options.from,
+        to: options.to,
+        subject: options.subject,
+        text: options.text,
+        html: options.html,
+        attachments: options.attachments
+      })
+
+      return right(options)
+    } catch (err) {
+      return left(new MailServiceError())
+    }
+  }
+}
